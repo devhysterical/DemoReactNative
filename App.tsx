08cd5630c9e479c16e4f5971f98bae9d5e6674cb
Dev.Hysterical <2124802010307@student.tdmu.edu.vn>
2025-04-17 10:55:25 +0700

    feat: Add Create New Account, Login, and Forgot Password screens with validation
    - Implemented CreateNewAccountScreen with email and password validation.
    - Added DemoAppbar and DemoAvatar components for UI consistency.
    - Created ForgotPasswordScreen for password reset functionality.
    - Developed LoginScreen with email and password validation, including handling for registered emails.
    - Integrated error handling and user feedback through alerts and helper texts.

--- App.tsx
+++ App.tsx
@@ -2,22 +2,54 @@
 // MSSV: 2124802010307
 
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
-import LoginScreen from './components/excersise_1_10042025/LoginScreen';
+// import {StyleSheet} from 'react-native';
+import {PaperProvider} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import LoginScreen from './components/buoi3_17042025/LoginScreen_1';
+import CreateNewAccountScreen from './components/buoi3_17042025/CreateNewAccountScreen';
+import ForgotPasswordScreen from './components/buoi3_17042025/ForgotPasswordScreen';
+
+export type RootStackParamList = {
+  Login: {registeredEmail?: string} | undefined;
+  CreateAccount: undefined;
+  ForgotPassword: {userId?: string};
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <LoginScreen />
-    </SafeAreaView>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          {/* Khai báo các màn hình trong Stack */}
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="CreateAccount"
+            component={CreateNewAccountScreen}
+            options={{title: 'Create New Account'}}
+          />
+          <Stack.Screen
+            name="ForgotPassword"
+            component={ForgotPasswordScreen}
+            options={{title: 'Reset Password'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
-
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//   },
+// });
 export default App;
